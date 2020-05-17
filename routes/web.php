76d9a7527d5d04b959df0adbe1ab35a0aad3aa2f
MD.HARUN-UR-RASHID <harun.cox@gmail.com>
2020-05-17 09:13:19 +0600

    update

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,12 @@
 <?php
-Route::macro('filemanagerRoutes',function(){
-    Route::group([
-        'namespace' => '\Haruncpi\LaravelSimpleFilemanager\Controllers',
-        'middleware' => config('filemanager.middleware')
-    ], function () {
-        Route::get(config('filemanager.base_route'), 'FilemanagerController@getIndex')
-            ->name('filemanager.base_route');
 
-        Route::post(config('filemanager.base_route'), 'FilemanagerController@postAction')
-            ->name('filemanager.action_route');
-    });
+Route::group([
+    'namespace'  => '\Haruncpi\LaravelSimpleFilemanager\Controllers',
+    'middleware' => config('filemanager.middleware')
+], function () {
+    Route::get(config('filemanager.base_route'), 'FilemanagerController@getIndex')
+        ->name('filemanager.base_route');
+
+    Route::post(config('filemanager.base_route'), 'FilemanagerController@postAction')
+        ->name('filemanager.action_route');
 });
